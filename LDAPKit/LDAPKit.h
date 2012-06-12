e7ce23712034f7492f32aa7dac1054eeed481c9b
David M. Syzdek <syzdek@bindlebinaries.com>
2012-06-11 16:38:28 -0800

    Adding initial Xcode project.

---
+++ LDAPKit/LDAPKit.h
@@ -0,0 +1,13 @@
+//
+//  LDAPKit.h
+//  LDAPKit
+//
+//  Created by David Syzdek on 6/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LDAPKit : NSObject
+
+@end
---
+++ LDAPKit/LDAPKit.m
@@ -0,0 +1,13 @@
+//
+//  LDAPKit.m
+//  LDAPKit
+//
+//  Created by David Syzdek on 6/11/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "LDAPKit.h"
+
+@implementation LDAPKit
+
+@end
