d3f9c3560247a65481e0815e9e03e2b197b0c16e
David M. Syzdek <syzdek@bindlebinaries.com>
2012-07-02 10:08:16 -0800

    Updating LKMessage properties to be atomic.

--- LdapKit/models/LKMessage.h
+++ LdapKit/models/LKMessage.h
@@ -149,7 +149,7 @@ typedef enum ldap_kit_ldap_message_type LKLdapMessageType;
 /// `LKLdapMessageTypeSearch` | LDAP search request
 /// `LKLdapMessageTypeUnbind` | LDAP unbind request
 /// `LKLdapMessageTypeWhoAmI` | LDAP whoami request
-@property (nonatomic, readonly) LKLdapMessageType messageType;
+@property (atomic, readonly) LKLdapMessageType messageType;
 
 
 #pragma mark - Errors
@@ -162,38 +162,38 @@ typedef enum ldap_kit_ldap_message_type LKLdapMessageType;
 @property (atomic, readonly) NSInteger errorCode;
 
 /// An optional title of the error for use when reporting error to users.
-@property (nonatomic, readonly) NSString * errorTitle;
+@property (atomic, copy, readonly) NSString * errorTitle;
 
 /// A human readable error message.
-@property (nonatomic, readonly) NSString * errorMessage;
+@property (atomic, copy, readonly) NSString * errorMessage;
 
 /// Additional diagnostic information if available.
-@property (nonatomic, readonly) NSString * diagnosticMessage;
+@property (atomic, copy, readonly) NSString * diagnosticMessage;
 
 /// Determines if the error code indicates whether the task succeeded or failed.
-@property (nonatomic, readonly) BOOL isSuccessful;
+@property (atomic, readonly) BOOL isSuccessful;
 
 
 #pragma mark - Results
 /// @name Results
 
 /// An array of LKEntry objects returned by a search request.
-@property (nonatomic, readonly) NSArray * entries;
+@property (atomic, copy, readonly) NSArray * entries;
 
 /// An array of LDAP referrals returned by an LDAP request.
-@property (nonatomic, readonly) NSArray * referrals;
+@property (atomic, copy, readonly) NSArray * referrals;
 
-@property (nonatomic, readonly) NSArray * matchedDNs;
+@property (atomic, copy, readonly) NSArray * matchedDNs;
 
 
 #pragma mark - Identifying the LKMessage
 /// @name Identifying the LKMessage
 
 /// An integer that you can use to identify messages in your application.
-@property (nonatomic, assign) NSInteger tag;
+@property (atomic, assign) NSInteger tag;
 
 /// An object reference you can use to associate data with messages in your
 /// application.
-@property (nonatomic, retain) id <NSObject> object;
+@property (atomic, retain) id <NSObject> object;
 
 @end
